ed7e06609090930acaef7436feb04b71606691d5
Marwan Wael El Zaher <m.wael@penta-b.com>
2024-12-02 18:09:02 +0200

    style: icons and its locaalization

--- src/messages.js
+++ src/messages.js
@@ -1,9 +1,12 @@
-import Icon from './plugin.svg';
-
+import Icon from './assets/plugin.svg';
+import MicOn from './assets/micon.svg';
+import MicOff from './assets/micoff.svg';
 
 export default {
     "click.msg": "You clicked on location: {0}, {1}",
     "test.plurals": "There {0,plural,one{is # apple}other{are # apples}} in {1,plural,one{# basket}other{# baskets}}.",
     "title": "ai-model-plugin",
-    "icon": Icon
+    icon: Icon,
+    deactivateMic: MicOff,
+    micActivated: MicOn
 }
