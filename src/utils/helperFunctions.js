7c5c703122a9c7e10ccfa7a0d57b452d1122bd03
Marwan Wael El Zaher <m.wael@penta-b.com>
2024-12-03 21:08:48 +0200

    feat: executeDataAction function as a helper function where it execute the query and do some actions based on the response if the length is zero a notification is appeard if 1 do the action more than one show a grid with the informations of the features with action buttons, created new zoomtoFeatures function where it is used to zoom in BBox instead of using convert geoGson to feature, added debounce function to handle the typing timeout of the userQuery text input

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -4,7 +4,7 @@ import {
   apiRegistry,
   actionsRegistry,
 } from "@penta-b/ma-lib";
-// import { buffer } from "@turf/turf";
+import { executeQuery } from './query';
 export const dispatch = actionsRegistry.dispatch.bind(actionsRegistry);
 export const storeDispatch = store.dispatch.bind(store);
 
@@ -19,9 +19,26 @@ export const GEOJSONToFeature = async (GEOJSON) => {
   });
 };
 
-export const zoomToFeature = (featRef) => {
-  dispatch("zoomToFeatures", featRef);
-};
+// export const zoomToFeature = (featRef) => {
+//   dispatch("zoomToFeatures", featRef);
+// };
+
+//zoom to Feature function by using feature bbox 
+export const zoomToFeature = (geometry) => {
+  apiRegistry
+    .getApis(["Feature"])
+    .then(([Feature]) => {
+      const feature = new Feature({
+        type: "Feature",
+        geometry: geometry,
+        properties: {},
+      });
+
+      actionsRegistry.dispatch("setMapBBOX", feature.getBBox());
+      actionsRegistry.dispatch("addHighlight", feature);
+    });
+}
+
 export const zoomIn = (zoomLevel) => {
   if (zoomLevel) {
     dispatch("setZoom", zoomLevel);
@@ -54,9 +71,7 @@ export const clearHighlights = () => {
 export const layerVisibility = (layerId, visibility) => {
   dispatch('setLayerVisibility', layerId, visibility);
 };
-// export const generateBuffer = (GEOJSON, distance_in_meters) => {
-//   return buffer(GEOJSON, distance_in_meters, { units: "meters" });
-// };
+
 
 
 export const purge = () => {
@@ -71,4 +86,78 @@ export const purge = () => {
 
 export const terminate = (componentId, removeComponent) => {
   componentId && removeComponent(componentId);
+};
+export const debounce = (func, wait) => {
+  let timeout; return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(this, args), wait);
+  };
+};
+
+const RESPONSE_TYPE = {
+  NONE: 'NONE',
+  SINGLE: 'SINGLE',
+  MULTIPLE: 'MULTIPLE'
+};
+const getResponseType = (response) => {
+  if (!response?.length) return RESPONSE_TYPE.NONE;
+  if (response.length == 1) return RESPONSE_TYPE.SINGLE;
+  return RESPONSE_TYPE.MULTIPLE;
+};
+
+const handleSingleFeature = (featureData, actionContext) => {
+  const parsedFeature = JSON.parse(featureData?.geom);
+  actionContext.tagertedFeatureRef = parsedFeature;
+  return parsedFeature;
+};
+
+const handleMultipleFeatures = (featuresData, setGridVisible, setResponse) => {
+  setGridVisible(true);
+  setResponse(featuresData);
+};
+
+export const handleQueryResponse = async (
+  queryResponse,
+  actionContext,
+  setGridVisible,
+  setResponse
+) => {
+  // Determine response type
+  const responseType = getResponseType(queryResponse);
+
+  switch (responseType) {
+    case RESPONSE_TYPE.NONE:
+      notify("No features found matching your query", "info");
+      return null;
+
+    case RESPONSE_TYPE.SINGLE:
+      return handleSingleFeature(queryResponse[0], actionContext);
+
+    case RESPONSE_TYPE.MULTIPLE:
+      return handleMultipleFeatures(queryResponse, setGridVisible, setResponse);
+
+    default:
+      console.error('Unknown response type');
+      return null;
+  }
+};
+
+
+/**
+ * Executes data query on a specfic layer, feature
+ * @param {*} action action object
+ */
+export const executeDataAction = async (action, actionContext, projection, setGridVisible, setResponse) => {
+  console.log(action, 'action', 'data')
+
+  if (action.action != 'query') return;
+
+  const mapProjection = projection?.code || action.parameters.crs;
+  const queryResponse = await executeQuery(
+    action.parameters.layerId,
+    mapProjection,
+    action.parameters.searchText
+  );
+  handleQueryResponse(queryResponse, actionContext, setGridVisible, setResponse);
+
 };
