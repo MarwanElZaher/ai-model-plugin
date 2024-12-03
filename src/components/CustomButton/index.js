024cc0c741489d599e84401dd4cec177695a53a7
Marwan Wael El Zaher <m.wael@penta-b.com>
2024-12-03 21:01:49 +0200

    feat: created custom button

---
+++ src/components/CustomButton/index.js
@@ -0,0 +1,16 @@
+import { ReactSVG } from "react-svg";
+import React from "react";
+const CustomButton = ({ onClick, label, iconKey, className, isDisabled }) => {
+    return (
+        <button
+            disabled={isDisabled}
+            onClick={onClick}
+            className={`penta-button ${className}`}
+            title={label}
+        >
+            {iconKey && <ReactSVG src={iconKey} />}
+            <label>{label}</label>
+        </button>
+    );
+};
+export default CustomButton
