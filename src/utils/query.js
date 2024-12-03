8bf4fd970d748121e4296d8f3382fd765ca13568
Marwan Wael El Zaher <m.wael@penta-b.com>
2024-12-03 21:04:01 +0200

    feat: handled query to be using findCandidates endpoint instead of queryFeature

--- src/utils/query.js
+++ src/utils/query.js
@@ -3,19 +3,25 @@ import {
     systemShowLoading,
     systemHideLoading,
     store,
+    request,
 } from '@penta-b/ma-lib';
+import { notify } from './helperFunctions';
 
 // Query Builder - Builds the query structure
-const buildQueryData = (dataSource, crs, returns, conditionList) => {
+const buildQueryData = (layerId, crs, searchText) => {
     try {
         return [{
-            dataSource: dataSource,
-            ...(conditionList ? {
-                filter: {
-                    conditionList: [conditionList]
+            dataSource: {
+                "systemLayer": true
+            },
+            layers: [
+                {
+                    id: layerId,
+                    searchField: "text"
                 }
-            } : {}),
-            returns: returns ?? [],
+            ],
+            limit: "10",
+            searchText: searchText,
             crs: crs,
         }];
     } catch (error) {
@@ -24,28 +30,47 @@ const buildQueryData = (dataSource, crs, returns, conditionList) => {
     }
 };
 
-// Query Service - Handles the API call
-const executeQueryService = async (queryData) => {
+const executeQueryEndPoint = async (queryData) => {
     store.dispatch(systemShowLoading());
-
     try {
-        const response = await query.queryFeatures(queryData);
-        return response;
-    } catch (error) {
-        console.error('Query service error:', error);
-        throw error;
+        const res = await request({
+            method: "post",
+            url: "/query/api/query/findCandidates",
+            data: [...queryData],
+        });
+
+        if (res.status == 200) {
+            console.log(
+                "Request To findCandidates Success and return with data "
+            );
+            const performedData = performSearchResult(res?.data);
+            store.dispatch(systemHideLoading());
+            return performedData;
+        }
+    } catch (event) {
+        console.error(
+            "Request To findCandidates FAIL and return with error - {} ",
+            event
+        );
+        store.dispatch(systemHideLoading());
+        notify("errorMessage", "error");
+        return null;
     } finally {
         store.dispatch(systemHideLoading());
+
     }
 };
+const performSearchResult = (resultArray) => {
+    return [...resultArray].filter((x) => x).flat(1);
+}
 
-export const executeQuery = async (dataSource, crs, returns, conditionList) => {
+export const executeQuery = async (layerId, crs, searchText) => {
     try {
         // Build query
-        const queryData = buildQueryData(dataSource, crs, returns, conditionList);
+        const queryData = buildQueryData(layerId, crs, searchText);
 
         // Execute query
-        const response = await executeQueryService(queryData);
+        const response = await executeQueryEndPoint(queryData);
 
         return response;
     } catch (error) {
@@ -53,3 +78,55 @@ export const executeQuery = async (dataSource, crs, returns, conditionList) => {
         throw error;
     }
 };
+
+// const buildQueryData = (dataSource, crs, returns, conditionList) => {
+//     try {
+//         return [{
+//             dataSource: dataSource,
+//             ...(conditionList ? {
+//                 filter: {
+//                     conditionList: [conditionList]
+//                 }
+//             } : {}),
+//             returns: returns ?? [],
+//             crs: crs,
+//         }];
+//     } catch (error) {
+//         console.error('Error building query:', error);
+//         throw error;
+//     }
+// };
+
+
+// export const executeQuery = async (dataSource, crs, returns, conditionList) => {
+//     try {
+//         // Build query
+//         const queryData = buildQueryData(dataSource, crs, returns, conditionList);
+
+//         // Execute query
+//         const response = await executeQueryService(queryData);
+
+//         return response;
+//     } catch (error) {
+//         console.error('Query execution failed:', error);
+//         throw error;
+//     }
+// };
+
+
+
+
+// Query Service - Handles the API call
+// const executeQueryService = async (queryData) => {
+//     store.dispatch(systemShowLoading());
+
+//     try {
+//         const response = await query.queryFeatures(queryData);
+//         return response;
+//     } catch (error) {
+//         console.error('Query service error:', error);
+//         throw error;
+//     } finally {
+//         store.dispatch(systemHideLoading());
+//     }
+// };
