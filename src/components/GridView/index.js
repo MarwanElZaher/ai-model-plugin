eafa6a96dc06f555f3a7e49f652421d0b0bf96d1
Marwan Wael El Zaher <m.wael@penta-b.com>
2024-12-04 16:16:12 +0200

    feat: added action buttons to grid , zoom highlight and clear

--- src/components/GridView/index.js
+++ src/components/GridView/index.js
@@ -1,73 +1,75 @@
-import React, { useEffect } from "react";
-import {
-    actionsRegistry,
-    withLocalize,
-    getLocale,
-    selectorsRegistry,
-    systemAddPrompt,
-    systemAddNotification,
-} from '@penta-b/ma-lib';
+import React from "react";
+import { withLocalize, getLocale } from '@penta-b/ma-lib';
 import { components as GridComponents } from '@penta-b/grid';
-import { trComponents } from '../../constants/gridButtons'
+import { trComponents } from '../../constants/gridButtons';
 import { connect } from "react-redux";
-import { GRID_FILTERABLE, GRID_PAGE_SIZE, GRID_PAGE_SIZE_OPTIONS, GRID_SORTABLE, LOCALIZATION_NAMESPACE } from "../../constants/constants";
+import {
+    GRID_FILTERABLE,
+    GRID_PAGE_SIZE,
+    GRID_PAGE_SIZE_OPTIONS,
+    GRID_SORTABLE,
+    LOCALIZATION_NAMESPACE
+} from "../../constants/constants";
+
 const GridView = ({ features, t }) => {
-    const featuresToPreview = features.map((feature) => {
-        const locale = getLocale() && getLocale().name;
-        return {
-            "Id": feature.id,
-            "Name": locale == "ar" ? feature.display_data_ar : feature.display_data_en,
-        }
-    })
-    const columns = [{
-        "id": "Id",
-        "name": t("Id"),
-        "type": "INT",
-        "display": "Basic"
+    const locale = getLocale()?.name;
 
-    }, {
-        "id": "Name",
-        "name": t("FeatureName"),
-        "type": "STRING",
-        "display": "Basic"
-
-    }]
-    const columnsConfig = columns.map((column) => {
-        return {
-            id: column.id,
-            name: column.name,
-            type: column.type,
-            display: column.display,
-            sortable: true,
-            filterable: false,
-            gridData: true
-        }
+    const mapFeatureToPreview = (feature) => ({
+        Id: feature.id,
+        Name: locale == "ar" ? feature.display_data_ar : feature.display_data_en,
+        geometry: JSON.parse(feature.geom)
     });
 
-    useEffect(() => {
-        console.log(features, 'features')
-    }, [features])
-    const newId = new Date().getUTCMilliseconds();
+    const getColumnsConfig = () => {
+        const baseColumns = [
+            {
+                id: "Id",
+                name: t("Id"),
+                type: "INT",
+                display: "Basic",
+                sortable: true,
+                filterable: false,
+                gridData: true
+            },
+            {
+                id: "Name",
+                name: t("FeatureName"),
+                type: "STRING",
+                display: "Basic",
+                sortable: true,
+                filterable: false,
+                gridData: true
+            },
+            {
+                id: "more",
+                name: t("Actions"),
+                type: "component",
+                display: "Basic",
+                filterable: false,
+                sortable: false
+            }
+        ];
+
+        return baseColumns;
+    };
+
+    const gridSettings = {
+        id: Date.now().toString(),
+        data: features.map(mapFeatureToPreview),
+        columns: getColumnsConfig(),
+        filterable: GRID_FILTERABLE,
+        sortable: GRID_SORTABLE,
+        pageSizeOptions: GRID_PAGE_SIZE_OPTIONS,
+        pageSiza: GRID_PAGE_SIZE,
+        language: locale
+    };
+
     return (
         <>
-
-            <div>
-                {t("Choose what Feature you want to see")}
-            </div>
+            <div>{t("Choose what Feature you want to see")}</div>
             <GridComponents.Grid
-                settings={{
-                    id: JSON.stringify(newId),
-                    data: featuresToPreview,
-                    columns: columnsConfig,
-                    filterable: GRID_FILTERABLE,
-                    sortable: GRID_SORTABLE,
-                    pageSizeOptions: GRID_PAGE_SIZE_OPTIONS,
-                    pageSiza: GRID_PAGE_SIZE,
-                    language: getLocale() && getLocale().name,
-
-                }}
+                settings={gridSettings}
                 trComponents={trComponents}
-
             />
         </>
     );
