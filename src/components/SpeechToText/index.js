bc5c43a4cb9698f6a4a73945ae2efb59670744aa
Marwan Wael El Zaher <m.wael@penta-b.com>
2024-12-11 19:09:20 +0200

    feat: commented the debounce , added keydown logic for starting and stoping recording and submitting userQuery

--- src/components/SpeechToText/index.js
+++ src/components/SpeechToText/index.js
@@ -3,7 +3,7 @@ import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognitio
 import { getLocale } from '@penta-b/ma-lib';
 import { withLocalize } from '@penta-b/ma-lib';
 import { LOCALIZATION_NAMESPACE } from '../../constants/constants';
-import { debounce } from '../../utils/helperFunctions';
+import { debounce, notify } from '../../utils/helperFunctions';
 import CustomButton from '../CustomButton';
 import { connect } from 'react-redux';
 const SpeechToText = ({ userQuery, setUserQuery, t }) => {
@@ -16,14 +16,18 @@ const SpeechToText = ({ userQuery, setUserQuery, t }) => {
         resetTranscript,
         browserSupportsSpeechRecognition
     } = useSpeechRecognition();
+    // const debouncedSetUserQuery = useCallback(
+    //     debounce((value) => {
+    //         setUserQuery(value);
+    //     }, 600),
+    //     []
+    // );
 
-
-    const debouncedSetUserQuery = useCallback(
-        debounce((value) => {
-            setUserQuery(value);
-        }, 600),
-        []
-    );
+    // useEffect(() => {
+    //     return () => {
+    //         debouncedSetUserQuery.cancel();
+    //     };
+    // }, [debouncedSetUserQuery]);
 
     useEffect(() => {
         if (transcript !== "" && transcript !== userQuery && !listening) {
@@ -48,14 +52,31 @@ const SpeechToText = ({ userQuery, setUserQuery, t }) => {
     const handleTyping = (e) => {
         const value = e.target.value;
         setInputValue(value);
-        debouncedSetUserQuery(value);
     };
 
-    useEffect(() => {
-        return () => {
-            debouncedSetUserQuery.cancel();
-        };
-    }, [debouncedSetUserQuery]);
+    //submit query
+    const handleQuerySubmission = () => {
+        if (inputValue == "") {
+            notify(t("please insert query"), "info");
+        } else {
+            setUserQuery(inputValue);
+            notify(`${t("processing your request")} ${inputValue}`, "info")
+            setInputValue("");
+        }
+
+    }
+
+    //handle key down
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            handleQuerySubmission();
+            return;
+        }
+        if (event.key === "Space") {
+            listening ? stopRecording() : startRecording();
+            return;
+        }
+    }
 
 
     if (!browserSupportsSpeechRecognition) {
@@ -68,10 +89,15 @@ const SpeechToText = ({ userQuery, setUserQuery, t }) => {
                 className='penta-textbox'
                 value={listening ? transcript : inputValue}
                 onChange={handleTyping}
+                onKeyDown={handleKeyDown}
                 placeholder="Describe desired map/data interaction"
             />
             <div className='penta-buttons-container'>
-                <CustomButton className={listening ? 'penta-button-svg-title penta-sub-button' : 'penta-button-svg-title penta-main-button'} onClick={listening ? stopRecording : startRecording} label={listening ? t("Disable-Mic") : t("Enable-mic")} title={listening ? t("Disable-Voice-Recognition") : t("Enable-Vocie-Recognition")} iconKey={listening ? t('deactivateMic') : t('activateMic')} />
+                {inputValue.length ?
+                    <CustomButton className="penta-button penta-main-button" handleKeyDown={handleKeyDown} onClick={handleQuerySubmission} label={t("Submit-Query")} title={t("Submit-Query")} iconKey={t('submitQuery')} />
+                    :
+                    <CustomButton handleKeyDown={handleKeyDown} className={listening ? 'penta-button-svg-title penta-sub-button' : 'penta-button-svg-title penta-main-button'} onClick={listening ? stopRecording : startRecording} label={listening ? t("Disable-Mic") : t("Enable-mic")} title={listening ? t("Disable-Voice-Recognition") : t("Enable-Vocie-Recognition")} iconKey={listening ? t('deactivateMic') : t('activateMic')} />
+                }
             </div>
         </div>
     );
